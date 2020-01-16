8dbb923a8a48ead8aefdf54c83089808c6d503cf
Marc Cizeron <marc@cizeron.me>
2020-01-16 20:11:18 +0100

    Fix bugs

--- app/controllers/power_station_controller.rb
+++ app/controllers/power_station_controller.rb
@@ -6,7 +6,19 @@ class PowerStationController < ApplicationController
   end
 
   def create
-    PowerStation.create!(name: params[:name])
+    begin
+      PowerStation.create!(name: power_station_params)
+      flash[:success] = "La station photovoltaique est créée !"
+    rescue StandardError => e
+      flash[:error] = e.message
+    end
+
     redirect_to root_path
+  end
+
+  private
+
+  def power_station_params
+    params.require(:name)
   end
 end
